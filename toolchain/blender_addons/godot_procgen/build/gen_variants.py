069409b2efa28272434f75a63f42ecb4e8c54a03
Dusten Sobotta <dusten@hey.com>
2023-02-27 00:42:10 -0800

    geometry node modifiers are now correctly being randomized and exported through the entire chain to godot

--- toolchain/blender_addons/godot_procgen/build/gen_variants.py
+++ toolchain/blender_addons/godot_procgen/build/gen_variants.py
@@ -18,6 +18,7 @@
 
 import os
 import pathlib
+import random
 import bpy
 from .. import core
 from ..core import buildstep
@@ -34,8 +35,42 @@ class GenVariants(core.buildstep.BuildStep):
         core.utils.bl_open_file(operand)
         core.utils.bl_save_as_file(core.utils.create_tmp_blend())
 
-    def run(self) -> bool:
+    def print_inputs(self, geo_node: bpy.types.NodesModifier):
+        modifier_name = geo_node.name
+        print("GDPG modifier: " + modifier_name)
+        for node_input in geo_node.node_group.inputs:
+            identifier = node_input.identifier
+            is_int = isinstance(node_input, bpy.types.NodeSocketInterfaceInt)
+            is_float = isinstance(node_input, bpy.types.NodeSocketInterfaceFloat)
+            if is_int or is_float:
+                input_name = node_input.name
+                value = bpy.context.object.modifiers[modifier_name].get(identifier)
+                print(input_name + " -- " + str(value))
 
+    def randomize_inputs(self, geo_node: bpy.types.NodesModifier):
+        modifier_name = geo_node.name
+        print("GDPG randomizing inputs of modifier: " + modifier_name)
+        for node_input in geo_node.node_group.inputs:
+            identifier = node_input.identifier
+            #input_name = node_input.name
+            if isinstance(node_input, bpy.types.NodeSocketInterfaceInt):
+                min_value = node_input.min_value
+                max_value = node_input.max_value
+                if min_value != max_value:
+                    value = random.randrange(min_value, max_value)
+                    #print(input_name + " -- " + str(value))
+                    bpy.context.object.modifiers[modifier_name][identifier] = value
+                continue
+            if isinstance(node_input, bpy.types.NodeSocketInterfaceFloat):
+                min_value = node_input.min_value
+                max_value = node_input.max_value
+                if min_value != max_value:
+                    value = random.uniform(min_value, max_value)
+                    #print(input_name + " -- " + str(value))
+                    bpy.context.object.modifiers[modifier_name][identifier] = value
+                continue
+
+    def run(self) -> bool:
         variations = 5
         out_dir = os.path.join(core.utils.get_variants_dir(), self.__rel_path, self.__base_name)
         core.utils.create_dir(out_dir)
@@ -52,13 +87,13 @@ class GenVariants(core.buildstep.BuildStep):
                         #todo: handle multiple exporting multiple invariant objects from one source file
                         for obj in bpy.data.objects:
                             obj.select_set(True)
-                            bpy.ops.object.mode_set(mode='OBJECT')
-
                             for i in range(variations):
-                                #perform variation logic
+                                for modifier in bpy.context.object.modifiers:
+                                    if isinstance(modifier, bpy.types.NodesModifier):
+                                        self.randomize_inputs(modifier)
+                                        self.print_inputs(modifier)
                                 path = os.path.join(out_dir, self.__base_name + str(i+1) + ".blend")
                                 core.utils.bl_save_as_file(path)
-
                             obj.select_set(False)
 
         self._cleanup()
